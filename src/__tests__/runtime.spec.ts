bdda36a09a8af1ee488e7642f334e7eb47c552ba
Lukas Bombach <lukas.bombach@googlemail.com>
2023-07-24 10:55:43 +0200

    more test cases

--- src/__tests__/runtime.spec.ts
+++ src/__tests__/runtime.spec.ts
@@ -8,6 +8,8 @@ describe("value", () => {
     expect(get()).toBe(0);
     set(1);
     expect(get()).toBe(1);
+    set(2);
+    expect(get()).toBe(2);
   });
 
   test("it should trigger a reaction when the value is set", () => {
@@ -24,5 +26,22 @@ describe("value", () => {
     react(reaction);
     set({ a: 1 });
     expect(reaction).toHaveBeenCalledTimes(2);
+  });
+
+  xtest("it should not trigger a reaction when the value is set to the same value", () => {
+    const [get, set] = value(0);
+    const reaction = jest.fn(() => get());
+    react(reaction);
+    set(0);
+    expect(reaction).toHaveBeenCalledTimes(1);
+  });
+
+  test("a reaction can be triggered multiple times", () => {
+    const [get, set] = value(0);
+    const reaction = jest.fn(() => get());
+    react(reaction);
+    set(1);
+    set(2);
+    expect(reaction).toHaveBeenCalledTimes(3);
   });
 });
